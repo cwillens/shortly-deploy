774c24dbb63824574fa4e681f084ffeee05ca14a
Hack Reactor Students <accounts@hackreactor.com>
2016-10-12 20:31:38 -0700

    testing

--- server-config.js
+++ server-config.js
@@ -22,15 +22,15 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// app.get('/', util.checkUser, handler.renderIndex);
+app.get('/', util.checkUser, handler.renderIndex);
 // app.get('/create', util.checkUser, handler.renderIndex);
 
 // app.get('/links', util.checkUser, handler.fetchLinks);
 // app.post('/links', handler.saveLink);
 
-// app.get('/login', handler.loginUserForm);
-// app.post('/login', handler.loginUser);
-// app.get('/logout', handler.logoutUser);
+app.get('/login', handler.loginUserForm);
+app.post('/login', handler.loginUser);
+app.get('/logout', handler.logoutUser);
 
 app.get('/signup', handler.signupUserForm);
 app.post('/signup', handler.signupUser);
