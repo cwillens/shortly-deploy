3d334d840a0ceef524a660f1b9587b3eba7b561a
Hack Reactor Students <accounts@hackreactor.com>
2016-10-12 20:20:37 -0700

    Explosion

--- server-config.js
+++ server-config.js
@@ -22,19 +22,19 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.get('/', util.checkUser, handler.renderIndex);
-app.get('/create', util.checkUser, handler.renderIndex);
+// app.get('/', util.checkUser, handler.renderIndex);
+// app.get('/create', util.checkUser, handler.renderIndex);
 
-app.get('/links', util.checkUser, handler.fetchLinks);
-app.post('/links', handler.saveLink);
+// app.get('/links', util.checkUser, handler.fetchLinks);
+// app.post('/links', handler.saveLink);
 
-app.get('/login', handler.loginUserForm);
-app.post('/login', handler.loginUser);
-app.get('/logout', handler.logoutUser);
+// app.get('/login', handler.loginUserForm);
+// app.post('/login', handler.loginUser);
+// app.get('/logout', handler.logoutUser);
 
 app.get('/signup', handler.signupUserForm);
 app.post('/signup', handler.signupUser);
 
-app.get('/*', handler.navToLink);
+// app.get('/*', handler.navToLink);
 
 module.exports = app;
